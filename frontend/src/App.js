2b1fcf1f6e5141b0733e5e1be523658f50cc0dca
shakti1916 <shaktimaddeshiya34321@gmail.com>
2022-12-24 00:07:51 +0530

    video-3-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+      <a href="/">amazona</a>
+        
+      </header>
+      <main>
+        list product
+      </main>
+    </div>
+  );
+}
+
+export default App;
